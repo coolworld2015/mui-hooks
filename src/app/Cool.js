fefb851464c168862faaed64d5896e08630a8120
coolworld2015 <wintermute2015@ukr.net>
2022-01-01 22:13:57 +0200

    refactoring

--- src/app/Cool.js
+++ src/app/Cool.js
@@ -1,19 +1,15 @@
 import React, {useContext, useEffect, useState} from "react";
 import {Redirect} from 'react-router';
 import {AppContext} from "./index";
-import Avatar from '@mui/material/Avatar';
+
+import CoolItem from "./CoolItem";
 
 const Cool = () => {
     const [data, setData] = useState([]);
-    const [text, setText] = useState('REST API');
-    //const URL = 'http://ui-base.herokuapp.com/api/users/get';
-    //const URL = 'https://itunes.apple.com/search?media=&term=cool';
     const {item, setContextItem} = useContext(AppContext);
 
     useEffect(() => {
-        //setContextItem({...item,...{name: 'Cool'}});
         setData([]);
-        console.log('searchText - ', item.searchText)
         fetch('https://itunes.apple.com/search?media=&term=' + item.searchText, {
             method: 'get',
             headers: {
@@ -33,8 +29,6 @@ const Cool = () => {
             })
     }, [item.searchText]);
 
-    console.log('data - ', data)
-
     let loading = null;
 
     if (data.length < 1 || data === undefined) {
@@ -48,47 +42,16 @@ const Cool = () => {
                 textAlign: "center",
                 marginTop:'80px',
             }}>
-{/*                {text}
-                <br/>
-                <input value={text} onChange={(e) => setText(e.target.value)}/>
-                <hr/>*/}
+
                 {loading}
 
                 {
-                    data.map(user => (
-                            <User user={user} key={user.trackId}/>
+                    data.map(item => (
+                            <CoolItem obj={item} key={item.trackId}/>
                         )
                     )}
             </div>
         </>
-    )
-};
-
-const User = ({user}) => {
-    const [isClicked, setIsClicked] = useState(false);
-    const {item, setContextItem} = useContext(AppContext);
-
-    const clickHandler = (event) => {
-        event.preventDefault();
-        setContextItem({...item,...{name: 'Cool', item: user}});
-        setIsClicked(true)
-    };
-
-    if (isClicked) {
-        return <Redirect to="/cool_edit"/>
-    }
-
-    return (
-        <div style={{padding: '20px', marginTop:'0px', border: '1px solid #cccc'}}
-             onClick={(e) => clickHandler(e)}>
-            <img
-                src={user.artworkUrl100.replace('100x100bb.jpg', '500x500bb.jpg')}
-                width="189" height="255"
-            />
-            <br/>
-            {user.trackName} - {user.releaseDate.split('-')[0]} - {user.primaryGenreName}
-
-        </div>
     )
 };
 
